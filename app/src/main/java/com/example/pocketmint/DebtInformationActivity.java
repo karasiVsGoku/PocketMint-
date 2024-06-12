205490da5472359b0a577a621f11576eb0ac9e1b
Ba31nga <mynameisnati135@gmail.com>
2024-06-12 13:38:40 +0300

    Final Commit

--- app/src/main/java/com/example/pocketmint/DebtInformationActivity.java
+++ app/src/main/java/com/example/pocketmint/DebtInformationActivity.java
@@ -1,6 +1,10 @@
 package com.example.pocketmint;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class DebtInformationActivity extends AppCompatActivity {
+
+    private final Calendar calendar = Calendar.getInstance();
+    private EditText datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,41 @@ public class DebtInformationActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        datePicker = findViewById(R.id.date_input);
+        selectedDate();
+
+
     }
+
+    private void selectedDate() {
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                datePicker.setText(updateDate());
+
+            }
+        };
+
+        datePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(DebtInformationActivity.this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private String updateDate() {
+        String format = "yyyy-MM-dd";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format, Locale.getDefault());
+        return dateFormat.format(calendar.getTime());
+    }
+
+
+
 }
